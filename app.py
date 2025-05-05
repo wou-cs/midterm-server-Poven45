17f05c671efca4dc23f601eda1465dc36c3b0bdf
poven45 <povensangha@ymail.com>
2025-05-04 20:31:48 -0700

    added the api method so it returns 400 for everything not an int, also decrements and converts to hex

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
-
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route("/api/calcs/<number>", methods=["GET"])
+def calculate(number):
+    try:
+        num = int(number)
+        if num <= 0:
+            return "", 404
+        
+        return {
+            "dec": num - 1,
+            "hex": hex(num)
+        }
+    except ValueError:
+        return "", 400
